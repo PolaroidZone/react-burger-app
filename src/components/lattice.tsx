f4412a5cc1a54e20f515a1a320fe0e25d32e2673
Tsogang Mosweswe <tsogangamosweswe@gmail.com>
2023-08-14 18:38:54 +0200

    Co-authored-by: TASephapo <TASephapo@users.noreply.github.com>

---
+++ src/components/lattice.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/lattice.css'
+
+const lattice =() => {
+  return (
+    <div className='lattice'>
+      <p>Lattice</p>
+    </div>
+  )
+}
+
+export default lattice
